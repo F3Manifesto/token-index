fec90932f051ef861cd11c75d1867f5cfe6205ce
Emma-Jane MacKinnon-Lee <72898465+emmajane1313@users.noreply.github.com>
2022-04-27 08:19:48 -0400

    add

--- market/721/index.vue
+++ market/721/index.vue
@@ -1,2 +1,23 @@
-// The Interstitials 
-29, 30 //
+// The Interstitials: 29, 30, 47, 48, 49, 50, 51, 52
+
+// Web3 Fashion Originals: 53
+
+// The CC0 Arcade: 54
+
+// Dezinformatsiya: 55
+
+// Self Sovereign Essentials: 56
+
+// Hyperutility Packs: 57, 58
+
+// ZK Zeitgeist: 59, 60
+
+// Zero's Closet: 61, 62, 63, 64, 65, 66, 67
+
+// Edit's Closet: 68, 69, 70, 71, 72, 73, 74
+
+// Znarky: 75
+
+// The Navigator Chronicles: 76
+
+// My Pocket Empire: 77, 78
